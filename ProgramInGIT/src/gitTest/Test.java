6ab0924d268b8a93f36fcac6a5b276daaa90474e
rahul741591 <50498041+rahul741591@users.noreply.github.com>
2020-03-29 12:44:07 +0530

    3rd comment

--- ProgramInGIT/src/gitTest/Test.java
+++ ProgramInGIT/src/gitTest/Test.java
@@ -5,9 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("1St git test pass");
 		System.out.println("2nd git sucessfully");
-		System.out.println(" pass");
-		System.out.println("Fail");
-		System.out.println("Rahul pass");
+		System.out.println("Again pass");
+	   
 
 	}
 
