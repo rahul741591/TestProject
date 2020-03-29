0fe9f5dbc8f0f3860946471d64d30cbc937d6392
rahul741591 <50498041+rahul741591@users.noreply.github.com>
2020-03-29 12:46:31 +0530

    change

--- ProgramInGIT/src/gitTest/Test.java
+++ ProgramInGIT/src/gitTest/Test.java
@@ -5,10 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("1St git test pass");
 		System.out.println("2nd git sucessfully");
-		System.out.println("Again pass");
-		System.out.println("3rd passs");
 	   
-
 	}
 
 }
